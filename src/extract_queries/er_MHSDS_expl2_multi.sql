d5350c76cfa2de634cb7773571bb8c343b6bf75b
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-09 14:45:43 +0100

    Further development to exploration script.

--- src/extract_queries/er_MHSDS_expl2_multi.sql
+++ src/extract_queries/er_MHSDS_expl2_multi.sql
@@ -1,10 +1,32 @@
 
 -- Script for exploring aggregated count data from MHSDS across multiple months
 
+--- Dropping the temporary table if ruh multiple times in one session
+
+    DROP TABLE #HospOrder;
+    
+--- Creating a temporary table
+
 SELECT
-m.EthnicCategory,
-m.OrgIDProv,
-m.OrgIDCCGRes,
+    h.UniqMonthID,
+    h.Der_Person_ID,
+    f.ReportingPeriodStartDate,
+    f.ReportingPeriodEndDate,
+    h.RecordNumber,
+    h.UniqServReqID,
+    h.UniqHospProvSpellID,
+    h.StartDateHospProvSpell,
+    h.DischDateHospProvSpell,
+    ROW_NUMBER() OVER (PARTITION BY h.Der_Person_ID, h.UniqServReqID, h.UniqHospProvSpellID ORDER BY h.UniqMonthID DESC) AS Der_HospSpellRecordOrder
+INTO #HospOrder
+FROM [NHSE_MHSDS].[dbo].[MHS501HospProvSpell] h
+INNER JOIN [NHSE_MHSDS].[dbo].[MHSDS_SubmissionFlags] f ON h.NHSEUniqSubmissionID = f.NHSEUniqSubmissionID AND f.Der_IsLatest = 'Y'
+WHERE h.UniqMonthID BETWEEN 1462 AND 1464; -- Jan to Mar 2022
+
+
+--- Querying our temporary table
+
+SELECT
 SUM(CASE WHEN h.StartDateHospProvSpell BETWEEN h.ReportingPeriodStartDate AND h.ReportingPeriodEndDate THEN 1 ELSE 0 END) AS Admissions,
 COUNT(DISTINCT CASE WHEN h.StartDateHospProvSpell BETWEEN h.ReportingPeriodStartDate AND h.ReportingPeriodEndDate THEN h.Der_Person_ID END) AS PeopleAdmitted,
 SUM(CASE WHEN h.DischDateHospProvSpell BETWEEN h.ReportingPeriodStartDate AND h.ReportingPeriodEndDate THEN 1 ELSE 0 END) AS Discharges,
@@ -12,6 +34,4 @@ COUNT(DISTINCT CASE WHEN h.DischDateHospProvSpell BETWEEN h.ReportingPeriodStart
 
 FROM #HospOrder h
 
-INNER JOIN [NHSE_MHSDS].[dbo].[MHS001MPI] m ON m.RecordNumber = h.RecordNumber 
-
-WHERE h.Der_HospSpellRecordOrder = 1
+WHERE h.Der_HospSpellRecordOrder = 1;
