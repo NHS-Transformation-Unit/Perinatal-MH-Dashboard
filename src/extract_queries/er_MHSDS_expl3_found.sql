58481e0a53bf873d66c27d3aab4455c51eea6e35
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-23 11:02:42 +0100

    Starting the foundation of the final SQL script for pulling all data into one single MHSDS data source.

---
+++ src/extract_queries/er_MHSDS_expl3_found.sql
@@ -0,0 +1,27 @@
+
+-- Script for exploring joins between MHS101 and MHS102
+
+--- Foundation script for identifying all columns/tables we need to pull into one MHSDS dashboard data source
+
+SELECT
+    r.UniqMonthID, 
+    r.Der_Person_ID, 
+    f.ReportingPeriodStartDate,
+    f.ReportingPeriodEndDate,
+    r.RecordNumber, 
+    r.UniqServReqID, 
+    h.UniqHospProvSpellID,
+    h.StartDateHospProvSpell,
+    h.DischDateHospProvSpell,
+    s.ServTeamTypeRefToMH,
+    r.OrgIDProv,
+    d.EthnicCategory,
+    d.Gender
+    
+FROM [NHSE_MHSDS].[dbo].[MHS101Referral] r
+
+INNER JOIN [NHSE_MHSDS].[dbo].[MHSDS_SubmissionFlags] f ON r.NHSEUniqSubmissionID = f.NHSEUniqSubmissionID AND f.Der_IsLatest = 'Y'
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS501HospProvSpell] h ON r.NHSEUniqSubmissionID = h.NHSEUniqSubmissionID
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS102ServiceTypeReferredTo] s ON r.RecordNumber = s.RecordNumber AND r.UniqServReqID = s.UniqServReqID
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS001MPI] d ON r.RecordNumber = d.RecordNumber
+WHERE r.UniqMonthID BETWEEN 1462 AND 1464; -- Jan to Mar 2022
