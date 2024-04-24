cd282395f92e724de605dc3d862e8d31fc475c34
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-24 11:59:54 +0100

    Adding in postcode data, as well as numerous joins to description of coded columns.

--- src/extract_queries/er_MHSDS_expl3_found.sql
+++ src/extract_queries/er_MHSDS_expl3_found.sql
@@ -12,13 +12,17 @@ SELECT TOP (100)
     r.ReferralRequestReceivedDate,
     r.SourceOfReferralMH,
     r.OrgIDProv,
+    ro.ODS_Prov_orgName, 
     f.ReportingPeriodStartDate,
     f.ReportingPeriodEndDate,
     h.UniqHospProvSpellID,
     h.StartDateHospProvSpell,
     h.DischDateHospProvSpell,
     s.ServTeamTypeRefToMH,
+    rs.Main_Description,
     d.EthnicCategory,
+    re.Ethnic_Category_Main_Desc,
+    d.Postcode,
     --t.ReferToTreatPeriodStartDate, nearly all entries are NULL
     --t.ReferToTreatPeriodEndDate, nearly all entries are NULL
     o.OnwardReferDate,
@@ -33,6 +37,10 @@ LEFT JOIN [NHSE_MHSDS].[dbo].[MHS102ServiceTypeReferredTo] s ON r.RecordNumber =
 LEFT JOIN [NHSE_MHSDS].[dbo].[MHS001MPI] d ON r.RecordNumber = d.RecordNumber
 LEFT JOIN [NHSE_MHSDS].[dbo].[MHS104RTT] t ON r.RecordNumber = t.RecordNumber AND r.UniqServReqID = t.UniqServReqID
 LEFT JOIN [NHSE_MHSDS].[dbo].[MHS105OnwardReferral] o ON r.RecordNumber = o.RecordNumber AND r.UniqServReqID = o.UniqServReqID
+
+LEFT JOIN [NHSE_Reference].[dbo].[tbl_Ref_DataDic_ZZZ_EthnicCategory] re ON d.EthnicCategory = re.EthnicCategory
+LEFT JOIN [NHSE_Reference].[dbo].[tbl_Ref_DataDic_ZZZ_ServiceOrTeamTypeForMentalHealth] rs ON s.ServTeamTypeRefToMH = rs.Main_Code_Text
+LEFT JOIN [NHSE_Reference].[dbo].[tbl_Ref_ODS_ProviderSite] ro ON r.OrgIDProv = ro.ODS_Prov_OrgCode
 
 WHERE r.UniqMonthID BETWEEN 1477 AND 1488 -- Apr to May 2023/24
 AND r.OrgIDProv IN ('RV5', 'RPG', 'RQY') -- SLaM, Oxleas and SWLaSG
