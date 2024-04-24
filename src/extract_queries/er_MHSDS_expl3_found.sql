2ca0d2a696ac56e69f2f5665eedf2c5920dda7b2
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-24 11:35:38 +0100

    Fixed issue within joins that was returning a one-many error.

--- src/extract_queries/er_MHSDS_expl3_found.sql
+++ src/extract_queries/er_MHSDS_expl3_found.sql
@@ -3,7 +3,7 @@
 
 --- Foundation script for identifying all columns/tables we need to pull into one MHSDS dashboard data source
 
-SELECT TOP (100) -- Do I want to use DISTINCT to limit the number of Der_Persdon_ID I return?
+SELECT TOP (100)
     r.UniqMonthID, 
     r.Der_Person_ID, 
     r.AgeServReferRecDate,
@@ -28,13 +28,15 @@ SELECT TOP (100) -- Do I want to use DISTINCT to limit the number of Der_Persdon
 FROM [NHSE_MHSDS].[dbo].[MHS101Referral] r
 
 INNER JOIN [NHSE_MHSDS].[dbo].[MHSDS_SubmissionFlags] f ON r.NHSEUniqSubmissionID = f.NHSEUniqSubmissionID AND f.Der_IsLatest = 'Y'
-LEFT JOIN [NHSE_MHSDS].[dbo].[MHS501HospProvSpell] h ON r.NHSEUniqSubmissionID = h.NHSEUniqSubmissionID
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS501HospProvSpell] h ON r.RecordNumber = h.RecordNumber AND r.UniqServReqID = h.UniqServReqID
 LEFT JOIN [NHSE_MHSDS].[dbo].[MHS102ServiceTypeReferredTo] s ON r.RecordNumber = s.RecordNumber AND r.UniqServReqID = s.UniqServReqID
-LEFT JOIN [NHSE_MHSDS].[dbo].[MHS001MPI] d ON r.NHSEUniqSubmissionID = d.NHSEUniqSubmissionID
-LEFT JOIN [NHSE_MHSDS].[dbo].[MHS104RTT] t ON r.NHSEUniqSubmissionID = t.NHSEUniqSubmissionID
-LEFT JOIN [NHSE_MHSDS].[dbo].[MHS105OnwardReferral] o ON r.NHSEUniqSubmissionID = o.NHSEUniqSubmissionID
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS001MPI] d ON r.RecordNumber = d.RecordNumber
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS104RTT] t ON r.RecordNumber = t.RecordNumber AND r.UniqServReqID = t.UniqServReqID
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS105OnwardReferral] o ON r.RecordNumber = o.RecordNumber AND r.UniqServReqID = o.UniqServReqID
+
 WHERE r.UniqMonthID BETWEEN 1477 AND 1488 -- Apr to May 2023/24
 AND r.OrgIDProv IN ('RV5', 'RPG', 'RQY') -- SLaM, Oxleas and SWLaSG
 AND s.ServTeamTypeRefToMH = 'C02' -- Perinatal 
 AND r.AgeServReferRecDate BETWEEN 15 AND 44
-AND h.DischDateHospProvSpell IS NOT NULL; 
+
+ORDER BY r.Der_Person_ID DESC; 
