30c65a6622be36e29231e2ef66b5176ccbe82371
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-25 11:12:38 +0100

    Created a new script that removes the joins to NHSE_Refernce tables due to the time it was taking to process.

---
+++ src/extract_queries/er_MHSDS_expl4_noref.sql
@@ -0,0 +1,43 @@
+
+-- Script for exploring joins between the MHS101 Referral table and all other tables containing data needed for dashboard requirements
+
+--- Further development of the foundation script (`expl3`) but removing reference joins due to time taken to query
+
+SELECT DISTINCT TOP (20)
+r.UniqMonthID, 
+r.Der_Person_ID, 
+r.AgeServReferRecDate,
+r.RecordNumber, 
+r.UniqServReqID, 
+r.ReferralRequestReceivedDate,
+r.SourceOfReferralMH,
+r.OrgIDProv,
+f.ReportingPeriodStartDate,
+f.ReportingPeriodEndDate,
+h.UniqHospProvSpellID,
+h.StartDateHospProvSpell,
+h.DischDateHospProvSpell,
+s.ServTeamTypeRefToMH,
+d.EthnicCategory,
+d.DefaultPostcode,
+d.LSOA2011,
+d.IMDQuart,
+o.OnwardReferDate,
+o.OnwardReferReason,
+o.OrgIDReceiving
+
+FROM [NHSE_MHSDS].[dbo].[MHS101Referral] r
+
+INNER JOIN [NHSE_MHSDS].[dbo].[MHSDS_SubmissionFlags] f ON r.NHSEUniqSubmissionID = f.NHSEUniqSubmissionID AND f.Der_IsLatest = 'Y'
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS501HospProvSpell] h ON r.RecordNumber = h.RecordNumber AND r.UniqServReqID = h.UniqServReqID
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS102ServiceTypeReferredTo] s ON r.RecordNumber = s.RecordNumber AND r.UniqServReqID = s.UniqServReqID
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS001MPI] d ON r.RecordNumber = d.RecordNumber
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS104RTT] t ON r.RecordNumber = t.RecordNumber AND r.UniqServReqID = t.UniqServReqID
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS105OnwardReferral] o ON r.RecordNumber = o.RecordNumber AND r.UniqServReqID = o.UniqServReqID
+
+WHERE r.UniqMonthID BETWEEN 1477 AND 1488 -- Apr to May 2023/24
+AND r.OrgIDProv IN ('RV5', 'RPG', 'RQY') -- SLaM, Oxleas and SWLaSG
+AND s.ServTeamTypeRefToMH = 'C02' -- Perinatal 
+AND r.AgeServReferRecDate BETWEEN 15 AND 44
+
+ORDER BY r.Der_Person_ID DESC;
