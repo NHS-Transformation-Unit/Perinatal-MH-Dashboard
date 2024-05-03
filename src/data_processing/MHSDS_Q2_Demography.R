88ff5be7e11306eb657a4060c93300d255bea5f0
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-05-03 14:54:36 +0100

    Added in processing for the ethnicity data based on open referrals as a temporary dataset until caseload is working - will need changing in the long run.

---
+++ src/data_processing/MHSDS_Q2_Demography.R
@@ -0,0 +1,62 @@
+
+# Script for processing all demographic data related to the current caseload in perinatal MH services across English providers from April 2019 until February 2024
+
+## Loading Q2 data and data lookup files
+
+MHSDS_q2_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q3_NOC_Referrals.csv") ## will need changing to actual caseload data
+date_lookup <- paste0(here(),"/data/supporting_data/Date_Code_Lookup.csv")
+
+q2_raw_df <- read.csv(MHSDS_Q2_file_path)
+date_code_df <- read.csv(date_lookup)
+
+
+## Joining lookup file to raw Q3 data
+
+q2_dates_df <- left_join(q2_raw_df, date_code_df, by = c("UniqMonthID" = "Code"))
+
+
+## filtering to be only open cases (will need removing when changing to actual caseload data)
+
+q2_open_df <- Q2_dates_df %>%
+  filter(OpenReferrals == 1)
+
+
+## Filtering the raw Q2 data to isolate those living within most deprived quintile
+
+q2_dep_20 <- q2_open_df %>%
+  filter(IMD_Decile %in% c('1','2'))
+
+
+## Summarising the referral count per month for new referrals, based on the provider and ICB flags
+
+q2_eth_total_df <- q2_open_df %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
+  summarise(Referral_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Total",
+         Ethnicity = "All") %>%
+  select(1, 6, 7, 2, 3, 4, 5)
+
+q2_eth_spec_df <- q2_open_df %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, Ethnic_Category_Main_Desc) %>%
+  summarise(Referral_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Specific") %>%
+  mutate(Ethnicity = case_when(
+    Ethnic_Category_Main_Desc %in% c('British','Irish') ~ "WhiteBritishIrish",
+    Ethnic_Category_Main_Desc == 'Any other white background' ~ "OtherWhite",
+    Ethnic_Category_Main_Desc %in% c('White and Black Caribbean','White and Black African', 'White and Asian', 'Any other mixed background') ~ "EthnicityMixed",
+    Ethnic_Category_Main_Desc %in% c('Indian','Pakistani', 'Bangladeshi', 'Any other Asian background') ~ "AsianAsianBritish",
+    Ethnic_Category_Main_Desc %in% c('Caribbean','African', 'Any other Black background') ~ "BlackBlackBritish",
+    Ethnic_Category_Main_Desc %in% c('Chinese','Any other ethnic group') ~ "OtherEthnicGroups",
+    Ethnic_Category_Main_Desc == 'Not stated' ~ "NotStated",
+    Ethnic_Category_Main_Desc == 'Missing / invalid' ~ "MissingInvalid",
+    TRUE ~ "NotKnown")) %>%
+  select(1, 7, 8, 2, 3, 4, 6)
+
+q2_eth_combined <- rbind(q2_eth_total_df, q2_eth_spec_df)
+
+q2_eth_combined <- q2_eth_combined %>%
+  arrange(Month, Organisation_Name)
+
+write.csv(q2_eth_combined, paste0(here(),"/data/processed_extracts/MHSDS_Q2_Eth_Combined.csv"), row.names = FALSE)
