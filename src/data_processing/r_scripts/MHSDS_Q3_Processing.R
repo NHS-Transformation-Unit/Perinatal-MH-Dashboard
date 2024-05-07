6a7e101359178820410ae2e3ce03633275f86402
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-05-07 13:48:33 +0100

    Formatting of processing script.

--- src/data_processing/r_scripts/MHSDS_Q3_Processing.R
+++ src/data_processing/r_scripts/MHSDS_Q3_Processing.R
@@ -3,33 +3,33 @@
 
 ## Loading Q3 data and data lookup files
 
-MHSDS_Q3_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q3_NOC_Referrals.csv")
+MHSDS_q3_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q3_NOC_Referrals.csv")
 date_lookup <- paste0(here(),"/data/supporting_data/Date_Code_Lookup.csv")
 
-Q3_raw_df <- read.csv(MHSDS_Q3_file_path)
+q3_raw_df <- read.csv(MHSDS_q3_file_path)
 date_code_df <- read.csv(date_lookup)
 
 
 ## Joining lookup file to raw Q3 data
 
-Q3_dates_df <- left_join(Q3_raw_df, date_code_df, by = c("UniqMonthID" = "Code"))
+q3_dates_df <- left_join(q3_raw_df, date_code_df, by = c("UniqMonthID" = "Code"))
 
 
 ## Filtering the raw Q3 data into new, open and closed referral groups
 
-Q3_new_ref_df <- Q3_dates_df %>%
+q3_new_ref_df <- q3_dates_df %>%
   filter(NewReferrals == 1)
 
-Q3_open_ref_df <- Q3_dates_df %>%
+q3_open_ref_df <- q3_dates_df %>%
   filter(OpenReferrals == 1 & NewReferrals == 0)
 
-Q3_closed_ref_df <- Q3_dates_df %>%
+q3_closed_ref_df <- q3_dates_df %>%
   filter(ClosedReferrals == 1)
 
 
 ## Summarising the referral count per month for new referrals, based on the provider and ICB flags
 
-Q3_new_proc_df <- Q3_new_ref_df %>%
+q3_new_proc_df <- q3_new_ref_df %>%
   group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
   summarise(Referral_Count = n(), .groups = "drop") %>%
   rename(Organisation_Name = ODS_Prov_orgName) %>%
@@ -39,7 +39,7 @@ Q3_new_proc_df <- Q3_new_ref_df %>%
 
 ## Summarising the referral count per month for open referrals, based on the provider and ICB flags
 
-Q3_open_proc_df <- Q3_open_ref_df %>%
+q3_open_proc_df <- q3_open_ref_df %>%
   group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
   summarise(Referral_Count = n(), .groups = "drop") %>%
   rename(Organisation_Name = ODS_Prov_orgName) %>%
@@ -49,7 +49,7 @@ Q3_open_proc_df <- Q3_open_ref_df %>%
 
 ## Summarising the referral count per month for closed referrals, based on the provider and ICB flags
 
-Q3_closed_proc_df <- Q3_closed_ref_df %>%
+q3_closed_proc_df <- q3_closed_ref_df %>%
   group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
   summarise(Referral_Count = n(), .groups = "drop") %>%
   rename(Organisation_Name = ODS_Prov_orgName) %>%
@@ -59,7 +59,7 @@ Q3_closed_proc_df <- Q3_closed_ref_df %>%
 
 ## Combining all processed dataframes into one for exporting
 
-q3_proc_combined <- rbind(Q3_new_proc_df, Q3_open_proc_df, Q3_closed_proc_df)
+q3_proc_combined <- rbind(q3_new_proc_df, q3_open_proc_df, q3_closed_proc_df)
 
 q3_proc_combined <- q3_proc_combined %>%
   arrange(Month, Organisation_Name)
@@ -72,7 +72,7 @@ write.csv(q3_proc_combined, paste0(here(),"/data/processed_extracts/MHSDS_Q3_NOC
 
 ## Aggregating new referral sources
 
-Q3_ref_Source_df <- Q3_new_ref_df %>%
+q3_ref_Source_df <- q3_new_ref_df %>%
   group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, SourceOfReferralMH) %>%
   summarise(Referral_Count = n(), .groups = "drop") %>%
   rename(Organisation_Name = ODS_Prov_orgName) %>%
@@ -88,4 +88,4 @@ Q3_ref_Source_df <- Q3_new_ref_df %>%
   select(1, 7, 2, 3, 4, 5, 6) %>%
   arrange(Month, Organisation_Name)
 
-write.csv(Q3_ref_Source_df, paste0(here(),"/data/processed_extracts/MHSDS_Q3_Ref_Source.csv"), row.names = FALSE)
+write.csv(q3_ref_Source_df, paste0(here(),"/data/processed_extracts/MHSDS_Q3_Ref_Source.csv"), row.names = FALSE)
