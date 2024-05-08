7fe5e27d0496492832cd7000b66396bc1c078801
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-05-08 11:27:21 +0100

    Changes to Caseload script based on AW's progress.

--- src/extract_queries/sql_scripts/MHSDS_Q2_Caseload.sql
+++ src/extract_queries/sql_scripts/MHSDS_Q2_Caseload.sql
@@ -71,15 +71,13 @@ WITH OpenReferralData AS (
       MPI.[LSOA2011],
       IMD.[IMD_Decile],
       REF_PROS.[ODS_Prov_orgName],
-	  CARE.[CareContDate],
-	  CARE.[AttendOrDNACode],
 
       CASE WHEN (REF.[ServDischDate] IS NULL OR REF.[ServDischDate] > SF.[ReportingPeriodEndDate]) AND SERV.[ReferRejectionDate] IS NULL THEN 1 ELSE 0 END AS OpenReferrals,
       CASE WHEN REF.[ReferralRequestReceivedDate] BETWEEN SF.[ReportingPeriodStartDate] AND SF.[ReportingPeriodEndDate] THEN 1 ELSE 0 END AS NewReferrals,
       CASE WHEN REF.[ServDischDate] BETWEEN SF.[ReportingPeriodStartDate] AND SF.[ReportingPeriodEndDate] THEN 1 ELSE 0 END AS ClosedReferrals,
     
       CASE WHEN REF.[OrgIDProv] IN ('RV5', 'RPG', 'RQY') THEN 'Providers'
-		  ELSE 'Other' END AS [Provider_Flag],
+	    ELSE 'Other' END AS [Provider_Flag],
 	    CASE WHEN REF.[OrgIDProv] IN ('RRU', 'RPG', 'RJZ', 'RJ1', 'RV5', 'RJ2') THEN 'NHS South East London ICB'
 		  WHEN REF.[OrgIDProv] IN ('RJ7', 'RAX', 'RQY', 'RY9', 'RJ6', 'RVR') THEN 'NHS South West London ICB'
 		  ELSE 'Other' END AS [ICB_Flag]
@@ -119,10 +117,11 @@ WITH OpenReferralData AS (
   AND REF.[OrgIDProv] IN ('RV5', 'RPG', 'RQY')
   AND (MPI.[LADistrictAuth] IS NULL OR MPI.[LADistrictAuth] LIKE ('E%'))
   AND MPI.[Gender] = '2'
+  AND CareContDate > ReferralRequestReceivedDate
+  AND AttendOrDNACode IN ('5', '6')
+  
 )
 
 SELECT *
 FROM OpenReferralData
-WHERE OpenReferrals = 1
-AND CareContDate > ReferralRequestReceivedDate
-AND AttendOrDNACode IN ('5', '6');
+WHERE OpenReferrals = 1;
