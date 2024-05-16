7032899a69f5862ad1e0d0a76b63899fdc2bcd3a
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-05-16 13:57:03 +0100

    Development of an access script.

--- src/extract_queries/sql_scripts/MHSDS_Q1_Access.sql
+++ src/extract_queries/sql_scripts/MHSDS_Q1_Access.sql
@@ -1,59 +1,104 @@
 
--- Script to return data for metrics 1, 2, 3 and 4 (Perinatal MH service patient access)
+-- Script to return perinatal access for all English providers during the previous 12-months
 
-SELECT 
-    REF.[ReferralRequestReceivedDate],
-    REF_PROS.[ODS_Prov_orgName], 
-    CASE WHEN CARE.[AttendOrDNACode] = '5' THEN 'Arrived on time'
-         WHEN CARE.[AttendOrDNACode] = '6' THEN 'Arrived late, but was seen'
-         ELSE 'Other attendance status' END AS [AttendDesc],
-    CASE WHEN CARE.[ConsType] = '01' THEN 'F2F'
-         WHEN CARE.[ConsType] = '11' THEN 'Video'
-         ELSE 'Other consultation medium' END AS [ConsDesc],
-    COUNT(DISTINCT REF.[Der_Person_ID]) AS PatientCount
-    
+DECLARE @EndRP INT;
+
+SET @EndRP = (SELECT MAX(UniqMonthID)
+              FROM [NHSE_MHSDS].[dbo].[MHS101Referral]);
+
+DECLARE @StartRP INT;
+
+SET @StartRP = (SELECT MAX(UniqMonthID)
+                FROM [NHSE_MHSDS].[dbo].[MHS101Referral]
+                WHERE UniqMonthID <= DATEADD(MONTH, -24, @EndRP));
+
+SELECT DISTINCT
+REF.[UniqMonthID],
+SF.[ReportingPeriodStartDate],
+SF.[ReportingPeriodEndDate],
+REF.[Der_Person_ID], 
+REF.[AgeServReferRecDate],
+REF.[RecordNumber], 
+REF.[UniqServReqID], 
+REF.[ReferralRequestReceivedDate],
+REF.[SourceOfReferralMH],
+REF.[OrgIDProv],
+REF_PROS.[ODS_Prov_orgName], 
+SERV.[ServTeamTypeRefToMH],
+STYPE.[Main_Description],
+MPI.[EthnicCategory],
+ETH.[Ethnic_Category_Main_Desc],
+MPI.[LSOA2011],
+IMD.[IMD_Decile],
+CARE.[CareContDate],
+CARE.[AttendOrDNACode],
+CARE.[ConsMechanismMH],
+
+CASE WHEN CARE.[CareContDate] > SF.[ReportingPeriodEndDate] THEN 0 
+     ELSE 1 END AS [Count],
+
+CASE WHEN REF.[OrgIDProv] IN ('RV5', 'RPG', 'RQY') THEN 'Providers'
+		 ELSE 'Other' END AS [Provider_Flag],
+		      
+CASE WHEN REF.[OrgIDProv] IN ('RRU', 'RPG', 'RJZ', 'RJ1', 'RV5', 'RJ2') THEN 'NHS South East London ICB'
+		 WHEN REF.[OrgIDProv] IN ('RJ7', 'RAX', 'RQY', 'RY9', 'RJ6', 'RVR') THEN 'NHS South West London ICB'
+		 ELSE 'Other' END AS [ICB_Flag]
+
+INTO #tmp_ER_Access
 FROM [NHSE_MHSDS].[dbo].[MHS101Referral] AS REF
 
 INNER JOIN [NHSE_MHSDS].[dbo].[MHSDS_SubmissionFlags] AS SF
-    ON REF.[NHSEUniqSubmissionID] = SF.[NHSEUniqSubmissionID]
-    AND SF.[Der_IsLatest] = 'Y'
+ON REF.[NHSEUniqSubmissionID] = SF.[NHSEUniqSubmissionID]
+AND SF.[Der_IsLatest] = 'Y'
 
 LEFT JOIN [NHSE_MHSDS].[dbo].[MHS102ServiceTypeReferredTo] AS SERV
-    ON REF.[RecordNumber] = SERV.[RecordNumber] 
-    AND REF.[UniqServReqID] = SERV.[UniqServReqID]
-
-LEFT JOIN [NHSE_MHSDS].[dbo].[MHS201CareContact] AS CARE
-    ON REF.[RecordNumber] = CARE.[RecordNumber] 
-    AND REF.[UniqServReqID] = CARE.[UniqServReqID]
+ON REF.[RecordNumber] = SERV.[RecordNumber] AND REF.[UniqServReqID] = SERV.[UniqServReqID]
 
 LEFT JOIN [NHSE_MHSDS].[dbo].[MHS001MPI] AS MPI
-    ON REF.[RecordNumber] = MPI.[RecordNumber]
+ON REF.[RecordNumber] = MPI.[RecordNumber]
+
+LEFT JOIN [NHSE_Reference].[dbo].[tbl_Ref_DataDic_ZZZ_EthnicCategory] AS ETH
+ON MPI.[EthnicCategory] = ETH.[Ethnic_Category]
 
 LEFT JOIN [NHSE_Reference].[dbo].[tbl_Ref_DataDic_ZZZ_ServiceOrTeamTypeForMentalHealth] AS STYPE
-    ON SERV.[ServTeamTypeRefToMH] = STYPE.[Main_Code_Text]
-    AND STYPE.[Is_Latest] = '1'
+ON SERV.[ServTeamTypeRefToMH] = STYPE.[Main_Code_Text]
+AND STYPE.[Is_Latest] = '1'
 
 LEFT JOIN [NHSE_Reference].[dbo].[tbl_Ref_ODS_ProviderSite] AS REF_PROS
-    ON REF.[OrgIDProv] = REF_PROS.[ODS_Prov_OrgCode]
+ON REF.[OrgIDProv] = REF_PROS.[ODS_Prov_OrgCode]
 
-WHERE REF.[UniqMonthID] BETWEEN 1470 AND 1487
-    AND SERV.[ServTeamTypeRefToMH] = 'C02'
-    AND REF.[OrgIDProv] IN ('RV5', 'RPG', 'RQY')
-    AND (MPI.[LADistrictAuth] IS NULL OR MPI.[LADistrictAuth] LIKE 'E%')
-    AND MPI.[Gender] = '2'
-    AND CARE.[AttendOrDNACode] IN ('5', '6')
-    AND CARE.[ConsType] IN ('01', '11')
+LEFT JOIN [NHSE_Reference].[dbo].[tbl_Ref_Other_Deprivation_By_LSOA] AS [IMD]
+ON MPI.[LSOA2011] = IMD.[LSOA_Code]
+AND IMD.[Effective_Snapshot_Date] = '2019-12-31'
 
-GROUP BY 
-    REF.[ReferralRequestReceivedDate],
-    REF_PROS.[ODS_Prov_orgName],
-    CASE 
-        WHEN CARE.[AttendOrDNACode] = '5' THEN 'Arrived on time'
-        WHEN CARE.[AttendOrDNACode] = '6' THEN 'Arrived late, but was seen'
-        ELSE 'Other attendance status' 
-    END,
-    CASE 
-        WHEN CARE.[ConsType] = '01' THEN 'F2F'
-        WHEN CARE.[ConsType] = '11' THEN 'Video'
-        ELSE 'Other consultation medium' 
-    END;
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS201CareContact] AS CARE
+ON REF.[Der_Person_ID] = CARE.[Der_Person_ID] 
+AND REF.[UniqServReqID] = CARE.[UniqServReqID]
+
+LEFT JOIN [NHSE_MHSDS].[dbo].[MHS102ServiceTypeReferredTo] AS DISC
+ON REF.[RecordNumber] = DISC.[RecordNumber] 
+AND REF.[UniqServReqID] = DISC.[UniqServReqID]
+
+WHERE REF.[UniqMonthID] BETWEEN @StartRP AND @EndRP
+AND SERV.[ServTeamTypeRefToMH] = 'C02'
+AND REF.[OrgIDProv] IN ('RV5', 'RPG', 'RQY')
+AND (MPI.[LADistrictAuth] IS NULL OR MPI.[LADistrictAuth] LIKE ('E%'))
+AND MPI.[Gender] = '2'
+AND DISC.[ReferRejectionDate] IS NULL
+AND CARE.[ConsMechanismMH] IN ('01', '11')
+AND CARE.[AttendOrDNACode] IN ('5', '6')
+AND CARE.[CareContDate] IS NOT NULL
+
+
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY [UniqServReqID], [UniqMonthID] ORDER BY [CareContDate]) AS [Order]
+INTO #tmp_ER_Access_Order
+FROM #tmp_ER_Access
+
+SELECT *
+FROM #tmp_ER_Access_Order
+WHERE [Order] = 1
+AND [Count] = 1
+
+DROP TABLE #tmp_ER_Access
+DROP TABLE #tmp_ER_Access_Order;
