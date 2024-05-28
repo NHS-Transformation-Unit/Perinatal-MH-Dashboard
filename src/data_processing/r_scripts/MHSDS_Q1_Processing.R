ef68416293da6574305f18351ca8cbfbecac4e2c
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-05-28 14:59:59 +0100

    Drafting of the Q1 processing script.

--- src/data_processing/r_scripts/MHSDS_Q1_Processing.R
+++ src/data_processing/r_scripts/MHSDS_Q1_Processing.R
@@ -1,41 +1,54 @@
 
-# Script for prcessing the access to perinatal MH services across English providers from April 2019 until February 2024
+# Script for processing the access to perinatal MH services across English providers for previous 36-months
 
 
-## Loading Q4 data and data lookup files
+## Loading Q1 data and data lookup files
 
-MHSDS_q4_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q4_RTFc.csv")
+MHSDS_q1_main_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q1_Main_Access.csv")
+MHSDS_q1_snap_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q1_Snap_Access.csv")
 date_lookup <- paste0(here(),"/data/supporting_data/Date_Code_Lookup.csv")
 
-q4_raw_df <- read.csv(MHSDS_q4_file_path)
+q1_main_raw_df <- read.csv(MHSDS_q1_main_file_path)
+q1_snap_raw_df <- read.csv(MHSDS_q1_snap_file_path)
 date_code_df <- read.csv(date_lookup)
+
+
+## Filtering out of area patients
+
+q1_main_area_df <- q1_main_raw_df %>%
+  filter(SL_PRO_FLAG = 1)
+
+q1_snap_area_df <- q1_snap_raw_df %>%
+  filter(SL_PRO_FLAG = 1)
 
 
 ## Joining lookup file to raw Q4 data
 
-q4_dates_df <- left_join(q4_raw_df, date_code_df, by = c("UniqMonthID" = "Code"))
+q1_main_dates_df <- left_join(q1_main_area_df, date_code_df, by = c("UniqMonthID" = "Code"))
+q1_snap_dates_df <- left_join(q1_snap_area_df, date_code_df, by = c("UniqMonthID" = "Code"))
 
 
-## Summarising the referral count per month for new referrals, based on the provider and ICB flags
+## Summarising the access count per month based on the provider and ICB flags
 
-q4_RTFc_Partnership <- q4_dates_df %>%
-  group_by(Month, Provider_Flag) %>%
-  summarise(Average_RTFc = mean(Days_First_Contact, na.rm = TRUE), .groups = "drop") %>%
-  mutate(Metric = "Referral to First Contact", 
-         Organisation_Name = "Partnership Summary",
-         ICB_Flag = "Both") %>%
-  select(Month, Metric, Provider_Flag, ICB_Flag, Organisation_Name, Average_RTFc)
-
-q4_RTFc_Individual <- q4_dates_df %>%
+q1_main_proc_df <- q1_main_dates_df %>%
   group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
-  summarise(Average_RTFc = mean(Days_First_Contact, na.rm = TRUE), .groups = "drop") %>%
+  summarise(Referral_Count = n(), .groups = "drop") %>%
   rename(Organisation_Name = ODS_Prov_orgName) %>%
-  mutate(Metric = "Referral to First Contact") %>%
-  select(Month, Metric, Provider_Flag, ICB_Flag, Organisation_Name, Average_RTFc)
+  mutate(Metric = "Access",
+         Geography = "Provider Specific") %>%
+  select(1, 6, 2, 3, 4, 5)
 
-q4_RTFc_Summary <- rbind(q4_RTFc_Partnership, q4_RTFc_Individual)
+q1_snap_proc_df <- q1_snap_dates_df %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
+  summarise(Referral_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Access",
+         Geography = "National Snapshot") %>%
+  select(1, 6, 2, 3, 4, 5)
 
-q4_RTFc_Summary <- q4_RTFc_Summary %>%
+q1_access_summary <- rbind(q1_main_proc_df, q1_snap_proc_df)
+
+q1_access_summary <- q1_access_summary %>%
   arrange(Month, Organisation_Name)
 
-write.csv(q4_RTFc_Summary, paste0(here(),"/data/processed_extracts/MHSDS_Q4_RTFc.csv"), row.names = FALSE)
+write.csv(q1_access_summary, paste0(here(),"/data/processed_extracts/MHSDS_Q1_Access.csv"), row.names = FALSE)
