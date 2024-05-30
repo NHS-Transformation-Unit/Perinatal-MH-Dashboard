086a95e4f2519ed9cff2b94666074c1e27080204
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-05-30 11:17:35 +0100

    Q5 appointment and contact mechanism summary processing.

--- src/data_processing/r_scripts/MHSDS_Q5_Processing.R
+++ src/data_processing/r_scripts/MHSDS_Q5_Processing.R
@@ -1,15 +1,97 @@
---CASE WHEN CARE.[ConsMechanismMH] = '01' THEN 'Face to Face'
---   WHEN CARE.[ConsMechanismMH] = '02' THEN 'Telephone'
---   WHEN CARE.[ConsMechanismMH] = '03' THEN 'Telemedicine'
---   WHEN CARE.[ConsMechanismMH] = '04' THEN 'Talk type for a person unable to speak'
---   WHEN CARE.[ConsMechanismMH] = '05' THEN 'Email'
---   WHEN CARE.[ConsMechanismMH] = '06' THEN 'Short message service (SMS) - text messaging'
---   WHEN CARE.[ConsMechanismMH] = '07' THEN 'On-line Triage'
---   WHEN CARE.[ConsMechanismMH] = '08' THEN 'Online Instant Messaging'
---   WHEN CARE.[ConsMechanismMH] = '09' THEN 'Text message (Asynchronous)'
---   WHEN CARE.[ConsMechanismMH] = '10' THEN 'Instant messaging (Synchronous)'
---   WHEN CARE.[ConsMechanismMH] = '11' THEN 'Video Consultation'
---   WHEN CARE.[ConsMechanismMH] = '12' THEN 'Message Board (Asynchronous)'
---   WHEN CARE.[ConsMechanismMH] = '13' THEN 'Chat Room (Synchronous)'
---   WHEN CARE.[ConsMechanismMH] = '98' THEN 'Other'
---   ELSE 'Unknown' END AS [Consultation_Medium],
+
+# Script for processing the referral to first contact time for patients attending perinatal MH services across English providers from April 2019 until February 2024
+
+
+## Loading Q5 data and data lookup files
+
+MHSDS_q5_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q5_Appointments.csv")
+date_lookup <- paste0(here(),"/data/supporting_data/Date_Code_Lookup.csv")
+
+q5_raw_df <- read.csv(MHSDS_q5_file_path)
+date_code_df <- read.csv(date_lookup)
+
+
+## Filtering out of area patients
+
+q5_area_df <- q5_raw_df %>%
+  filter(SL_PRO_FLAG == 1)
+
+
+## Joining lookup file to raw Q4 data
+
+q5_dates_df <- left_join(q5_area_df, date_code_df, by = c("UniqMonthID" = "Code"))
+
+
+## Summarising the attendance status count per month for each provider
+
+q5_app_total_df <- q5_dates_df %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
+  summarise(Appointment_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Total",
+         Appointment_Status = "All") %>%
+  select(1, 6, 7, 2, 3, 4, 5)
+
+q5_app_spec_df <- q5_dates_df %>%
+  mutate(Appointment_Status = case_when(
+    AttendOrDNACode %in% c('5', '6')  ~ "Attended and seen",
+    AttendOrDNACode == '7'  ~ "Arrived late, not seen",
+    AttendOrDNACode %in% c('2', '4')  ~ "Appointment cancelled",
+    AttendOrDNACode == '3'  ~ "Did not attend",
+    TRUE ~ "NotKnown")) %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, Appointment_Status) %>%
+  summarise(Appointment_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Specific") %>%
+  select(1, 7, 5, 2, 3, 4, 6)
+
+q5_app_combined <- rbind(q5_app_total_df, q5_app_spec_df)
+
+q5_app_combined <- q5_app_combined %>%
+  arrange(Month, Organisation_Name)
+
+write.csv(q5_app_combined, paste0(here(),"/data/processed_extracts/MHSDS_Q5_App_Combined.csv"), row.names = FALSE)
+
+
+## Summarising the contact mechanism count per month for each provider
+
+q5_cont_df <- q5_dates_df
+  filter(AttendOrDNACode %in% c('5', '6'))
+
+q5_con_total_df <- q5_cont_df %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
+  summarise(Appointment_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Total",
+         Contact_Mech = "All") %>%
+  select(1, 6, 7, 2, 3, 4, 5)
+
+q5_con_spec_df <- q5_cont_df %>%
+  mutate(Contact_Mech = case_when(
+    ConsMechanismMH == '01'  ~ 'Face to Face',
+    ConsMechanismMH == '02'  ~ 'Telephone',
+    ConsMechanismMH == '03'  ~ 'Telemedicine',
+    ConsMechanismMH == '04'  ~ 'Talk type for a person unable to speak',
+    ConsMechanismMH == '05'  ~ 'Email',
+    ConsMechanismMH == '06'  ~ 'Short message service (SMS) - text messaging',
+    ConsMechanismMH == '07'  ~ 'On-line Triage',
+    ConsMechanismMH == '08'  ~ 'Online Instant Messaging',
+    ConsMechanismMH == '09'  ~ 'Text message (Asynchronous)',
+    ConsMechanismMH == '10'  ~ 'Instant messaging (Synchronous)',
+    ConsMechanismMH == '11'  ~ 'Video Consultation',
+    ConsMechanismMH == '12'  ~ 'Message Board (Asynchronous)',
+    ConsMechanismMH == '13'  ~ 'Chat Room (Synchronous)',
+    ConsMechanismMH == '98'  ~ 'Other',
+    TRUE ~ "NotKnown")) %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, Contact_Mech) %>%
+  summarise(Appointment_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Specific") %>%
+  select(1, 7, 5, 2, 3, 4, 6)
+
+q5_con_combined <- rbind(q5_con_total_df, q5_con_spec_df)
+
+q5_con_combined <- q5_con_combined %>%
+  arrange(Month, Organisation_Name)
+
+write.csv(q5_con_combined, paste0(here(),"/data/processed_extracts/MHSDS_Q5_Con_Combined.csv"), row.names = FALSE)
