ac7298cae3a4d2d4816d2cad919ad7347770924e
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-05-30 15:16:42 +0100

    Changes to Q5 script to allow for breaking attendance status down by demographics.

--- src/data_processing/r_scripts/MHSDS_Q5_Processing.R
+++ src/data_processing/r_scripts/MHSDS_Q5_Processing.R
@@ -96,3 +96,143 @@ q5_con_combined <- q5_con_combined %>%
   arrange(Month, Organisation_Name)
 
 write.csv(q5_con_combined, paste0(here(),"/data/processed_extracts/MHSDS_Q5_Con_Combined.csv"), row.names = FALSE)
+
+
+## Adding attendance categories for demography summarising
+
+q5_dem_spec_df <- q5_dates_df %>%
+  mutate(Appointment_Status = case_when(
+    AttendOrDNACode %in% c('5', '6') ~ "Attended and seen",
+    AttendOrDNACode == '7' ~ "Arrived late, not seen",
+    AttendOrDNACode == '2' ~ "Patient cancellation",
+    AttendOrDNACode == '4' ~ "Provider cancellation",
+    AttendOrDNACode == '3'  ~ "Did not attend",
+    TRUE ~ "NotKnown"))
+
+
+## Filtering the raw Q5 data to isolate those living within most deprived quintile
+
+q5_dep_20 <- q5_dem_spec_df %>%
+  filter(IMD_Decile %in% c('1','2'))
+
+q5_dep_40 <- q5_dem_spec_df %>%
+  filter(IMD_Decile %in% c('3','4'))
+
+q5_dep_60 <- q5_dem_spec_df %>%
+  filter(IMD_Decile %in% c('5','6'))
+
+q5_dep_80 <- q5_dem_spec_df %>%
+  filter(IMD_Decile %in% c('7','8'))
+
+q5_dep_100 <- q5_dem_spec_df %>%
+  filter(IMD_Decile %in% c('9','10'))
+
+
+## mutating the 'age' field to numeric
+
+q5_age_df <- q5_dem_spec_df %>%
+  mutate(AgeRepPeriodEnd = as.numeric(AgeRepPeriodEnd))
+
+
+## Creating a function to group by and summarise raW q2 data
+
+app_groupby_fct <- function(input, metric, cat_desc) {
+  result_df <- input %>%
+    group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, Appointment_Status) %>%
+    summarise(Referral_Count = n(), .groups = "drop") %>%
+    rename(Organisation_Name = ODS_Prov_orgName) %>%
+    mutate(Metric = metric,
+           IMD_Decile = cat_desc) %>%
+    select(Month, Metric, Provider_Flag, ICB_Flag, Organisation_Name, Appointment_Status, IMD_Decile, Referral_Count)
+  
+  return(result_df)
+  
+}
+
+
+## Summarising the caseload each month based on the ethnic group of referred patients
+
+q5_eth_total_df <- q5_dem_spec_df %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, Appointment_Status) %>%
+  summarise(Referral_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Total",
+         Ethnicity = "All") %>%
+  select(1, 7, 2, 3, 4, 5, 8, 6)
+
+q5_eth_spec_df <- q5_dem_spec_df %>%
+  mutate(Ethnicity = case_when(
+    Ethnic_Category_Main_Desc %in% c('British','Irish') ~ "WhiteBritishIrish",
+    Ethnic_Category_Main_Desc == 'Any other white background' ~ "OtherWhite",
+    Ethnic_Category_Main_Desc %in% c('White and Black Caribbean','White and Black African', 'White and Asian', 'Any other mixed background') ~ "EthnicityMixed",
+    Ethnic_Category_Main_Desc %in% c('Indian','Pakistani', 'Bangladeshi', 'Any other Asian background') ~ "AsianAsianBritish",
+    Ethnic_Category_Main_Desc %in% c('Caribbean','African', 'Any other Black background') ~ "BlackBlackBritish",
+    Ethnic_Category_Main_Desc %in% c('Chinese','Any other ethnic group') ~ "OtherEthnicGroups",
+    Ethnic_Category_Main_Desc == 'Not stated' ~ "NotStated",
+    Ethnic_Category_Main_Desc == 'Missing / invalid' ~ "MissingInvalid",
+    TRUE ~ "NotKnown")) %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, Appointment_Status, Ethnicity) %>%
+  summarise(Referral_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Specific") %>%
+  select(1, 8, 2, 3, 4, 5, 6, 7)
+
+q5_eth_combined <- rbind(q5_eth_total_df, q5_eth_spec_df)
+
+q5_eth_combined <- q5_eth_combined %>%
+  arrange(Month, Organisation_Name)
+
+write.csv(q5_eth_combined, paste0(here(),"/data/processed_extracts/MHSDS_Q5_Eth_Combined.csv"), row.names = FALSE)
+
+
+## Summarising the caseload each month based on the deprivation decile of referred patients
+
+q5_dep_total_df <- app_groupby_fct(q5_dem_spec_df, "Provider_Total", "All Quintiles (Deprivation)")
+q5_dep_20_df <- app_groupby_fct(q5_dep_20, "Provider_Specific", "First_Quintile (Deprivation)")
+q5_dep_40_df <- app_groupby_fct(q5_dep_40, "Provider_Specific", "Second_Quintile (Deprivation)")
+q5_dep_60_df <- app_groupby_fct(q5_dep_60, "Provider_Specific", "Third_Quintile (Deprivation)")
+q5_dep_80_df <- app_groupby_fct(q5_dep_80, "Provider_Specific", "Fourth_Quintile (Deprivation)")
+q5_dep_100_df <- app_groupby_fct(q5_dep_100, "Provider_Specific", "Fifth_Quintile (Deprivation)")
+
+q5_dep_combined <- rbind(q5_dep_total_df, 
+                         q5_dep_20_df,
+                         q5_dep_40_df,
+                         q5_dep_60_df,
+                         q5_dep_80_df,
+                         q5_dep_100_df)
+
+q5_dep_combined <- q5_dep_combined %>%
+  arrange(Month, Organisation_Name)
+
+write.csv(q5_dep_combined, paste0(here(),"/data/processed_extracts/MHSDS_Q5_Dep_Combined.csv"), row.names = FALSE)
+
+
+## Summarising the caseload each month based on the age group of referred patients
+
+q5_age_total_df <- q5_dem_spec_df %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, Appointment_Status) %>%
+  summarise(Referral_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Total",
+         Age_Band = "All") %>%
+  select( 1, 7, 2, 3, 4, 5, 8, 6)
+
+q5_age_spec_df <- q5_dem_spec_df %>%
+  mutate(Age_Band = case_when(
+    AgeRepPeriodEnd >= 16 & AgeRepPeriodEnd < 21 ~ "16-20",
+    AgeRepPeriodEnd >= 21 & AgeRepPeriodEnd < 26 ~ "21-25",
+    AgeRepPeriodEnd >= 26 & AgeRepPeriodEnd < 40 ~ "26-39",
+    AgeRepPeriodEnd >= 40 ~ "40+",
+    TRUE ~ "NotKnown")) %>%
+  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName, Appointment_Status, Age_Band) %>%
+  summarise(Referral_Count = n(), .groups = "drop") %>%
+  rename(Organisation_Name = ODS_Prov_orgName) %>%
+  mutate(Metric = "Provider_Specific") %>%
+  select(1, 8, 2, 3, 4, 5, 6, 7)
+
+q5_age_combined <- rbind(q5_age_total_df, q5_age_spec_df)
+
+q5_age_combined <- q5_age_combined %>%
+  arrange(Month, Organisation_Name)
+
+write.csv(q5_age_combined, paste0(here(),"/data/processed_extracts/MHSDS_Q5_Age_Combined.csv"), row.names = FALSE)
