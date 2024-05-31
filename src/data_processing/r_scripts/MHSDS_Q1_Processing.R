1772cf66f72e71513de96ec0c10ab68e9867a05e
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-05-31 09:46:25 +0100

    Removing Q1 Access national snapshot data for running through Tableau report instead.

--- src/data_processing/r_scripts/MHSDS_Q1_Processing.R
+++ src/data_processing/r_scripts/MHSDS_Q1_Processing.R
@@ -5,7 +5,6 @@
 ## Loading Q1 data and data lookup files
 
 MHSDS_q1_main_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q1_Access_Main.csv")
-MHSDS_q1_snap_file_path <- paste0(here(),"/data/raw_extracts/MHSDS_Q1_Access_Snap.csv")
 date_lookup <- paste0(here(),"/data/supporting_data/Date_Code_Lookup.csv")
 
 q1_main_raw_df <- read.csv(MHSDS_q1_main_file_path)
@@ -22,7 +21,6 @@ q1_main_area_df <- q1_main_raw_df %>%
 ## Joining lookup file to raw Q1 data
 
 q1_main_dates_df <- left_join(q1_main_area_df, date_code_df, by = c("UniqMonthID" = "Code"))
-q1_snap_dates_df <- left_join(q1_snap_raw_df, date_code_df, by = c("UniqMonthID" = "Code"))
 
 
 ## Summarising the access count per month based on the provider and ICB flags
@@ -35,17 +33,7 @@ q1_main_proc_df <- q1_main_dates_df %>%
          Geography = "Provider Specific") %>%
   select(1, 6, 2, 3, 4, 5)
 
-q1_snap_proc_df <- q1_snap_dates_df %>%
-  group_by(Month, Provider_Flag, ICB_Flag, ODS_Prov_orgName) %>%
-  summarise(Referral_Count = n(), .groups = "drop") %>%
-  rename(Organisation_Name = ODS_Prov_orgName) %>%
-  mutate(Metric = "Access",
-         Geography = "National Snapshot") %>%
-  select(1, 6, 2, 3, 4, 5)
-
-q1_access_summary <- rbind(q1_main_proc_df, q1_snap_proc_df)
-
-q1_access_summary <- q1_access_summary %>%
+q1_access_summary <- q1_main_proc_df %>%
   arrange(Month, Organisation_Name)
 
 write.csv(q1_access_summary, paste0(here(),"/data/processed_extracts/MHSDS_Q1_Access.csv"), row.names = FALSE)
