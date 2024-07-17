0211b52ef7b427115904f7798a0d697f1d535ea4
Simon Wickham <simon.wickham1@udal.nhs.uk>
2024-07-17 10:29:33 +0100

    Removal of openxlsx and readxl packacges and addition of odbc and DBI packages commit to packages.R

--- src/config/r_scripts/packages.R
+++ src/config/r_scripts/packages.R
@@ -2,12 +2,12 @@
 # Script for loading appropriate packages for the running of MHSDS processing scripts
 
 packages <- c("here",
-             "purrr",
-             "tidyverse",
-             "tidyr",
-             "stringr",
-             "openxlsx",
-             "readxl",
-             "dplyr")
+              "tidyverse",
+              "odbc",
+              "DBI",
+              "purrr",
+              "tidyr",
+              "stringr",
+              "dplyr")
 
 lapply(packages, library, character.only=TRUE)
