faf20cf79627c28b354b9841d838b927fdd61319
Simon Wickham <simon.wickham1@udal.nhs.uk>
2024-07-19 11:38:37 +0100

    Creation of code that runs all necessary scripts in RStudio to extract, process and export all data commit to MHSDS_Processing_Pipeline.R

---
+++ src/data_processing/r_scripts/udal_r_scripts/MHSDS_Processing_Pipeline.R
@@ -0,0 +1,24 @@
+
+# Script for running all processing scripts for the Perinatal Mental Health Dashboard pipeline
+
+library(here)
+
+## Loading necessary packages for processing scripts
+
+source(paste0(here(),"/src/config/r_scripts/packages.R"))
+
+## Remote access into the UDAL environment
+
+source(paste0(here(),"/src/config/r_scripts/UDAL_connect.R"))
+
+## Running of all processing scripts
+
+source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q1_Processing.R"))
+source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q2_Processing.R"))
+source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q3_Processing.R"))
+source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q4_Processing.R"))
+source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q5_Processing.R"))
+
+## Drop all temporary tables in UDAL
+
+source(paste0(here(),"/src/extract_queries/sql_scripts/udal_r_scripts/drop_temp_tables"))
