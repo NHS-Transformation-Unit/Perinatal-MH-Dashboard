08a86adbf76edd446af6b67a88ea38b50627a213
Simon Wickham <simon.wickham1@udal.nhs.uk>
2024-07-19 11:08:04 +0100

    Creation of script to drop all temp tables in UDAl environment commit to drop_temp_tables.sql

---
+++ src/extract_queries/sql_scripts/udal_sql_scripts/drop_temp_tables.sql
@@ -0,0 +1,10 @@
+
+-- Drop all temporary tables from the UDAL environment
+
+DROP TABLE #temp_Q1_Access
+DROP TABLE #temp_Q2_Caseload_Main_Order
+DROP TABLE #temp_Q2_Caseload_Main
+DROP TABLE #temp_Q2_Caseload_Snap_Order
+DROP TABLE #temp_Q2_Caseload_Snap
+DROP TABLE #temp_Q3_First_Contact_Order
+DROP TABLE #temp_Q3_First_Contact
