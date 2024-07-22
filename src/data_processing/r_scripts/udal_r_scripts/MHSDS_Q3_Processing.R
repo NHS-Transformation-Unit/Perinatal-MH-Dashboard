b7372e2d4df3e69445353fe5f2e0a7040ccd87fd
Simon Wickham <simon.wickham1@udal.nhs.uk>
2024-07-22 12:33:31 +0100

    correction of spelling in comment commit to MHSDS_Q3_Processing.R

--- src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q3_Processing.R
+++ src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q3_Processing.R
@@ -1,5 +1,5 @@
 
-# Script for prcessing all new, open and closed referrals to perinatal MH services across English providers for previous 36-months
+# Script for processing all new, open and closed referrals to perinatal MH services across English providers for previous 36-months
 
 ## Loading Q3 data and data lookup files
 
