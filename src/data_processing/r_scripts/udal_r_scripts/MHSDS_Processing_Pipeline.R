86208f3b894a12d3e7b3368a72b6ad606e05c35f
Simon Wickham <simon.wickham1@udal.nhs.uk>
2024-07-22 12:37:58 +0100

    Removal of MHSDS_Q1_Processing.R, as isn't needed in output, commit to MHSDS_Processing_Pipeline.R

--- src/data_processing/r_scripts/udal_r_scripts/MHSDS_Processing_Pipeline.R
+++ src/data_processing/r_scripts/udal_r_scripts/MHSDS_Processing_Pipeline.R
@@ -13,11 +13,10 @@ source(paste0(here(),"/src/config/r_scripts/UDAL_connect.R"))
 
 ## Running of all processing scripts
 
-source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q1_Processing.R"))
-source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q2_Processing.R"))
+source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q2_Processing.R"))    
 source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q3_Processing.R"))
-source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q4_Processing.R"))
-source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q5_Processing.R"))
+source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q4_Processing.R"))    
+source(paste0(here(),"/src/data_processing/r_scripts/udal_r_scripts/MHSDS_Q5_Processing.R"))    
 
 ## Drop all temporary tables in UDAL
 
